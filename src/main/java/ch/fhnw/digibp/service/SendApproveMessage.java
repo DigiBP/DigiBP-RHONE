3cbcb59674ae6cb5c55276c755fd45be059d5b9e
Sebastian Fix <sebastian.fix@onicial.ch>
2019-12-14 22:36:13 +0000

    Fixed Typo

--- src/main/java/ch/fhnw/digibp/service/SendApproveMessage.java
+++ src/main/java/ch/fhnw/digibp/service/SendApproveMessage.java
@@ -27,24 +27,13 @@ public class SendApproveMessage implements JavaDelegate {
     public void execute(DelegateExecution delegateExecution) throws Exception {
 
         final String baseUrl = "https://www.pactd.ch/api/registration/approve";
-
         URI uri = new URI(baseUrl);
-
-        String requestJson = "{\"Test\"}";
-
         RestTemplate restTemplate = new RestTemplate();
-
         HttpHeaders headers = new HttpHeaders();
-
         headers.setContentType(MediaType.APPLICATION_JSON);
-
         System.out.println(delegateExecution.getVariable("uuid"));
-
-        //String JO = "{\"uuid\" : " + delegateExecution.getVariable("uuid") + "}";
-
-        HttpEntity<String> request = new HttpEntity<String>(requestJson, headers);
-
+        String JO = "{\"uuid\" : \"" + delegateExecution.getVariable("uuid") + "\"}";
+        HttpEntity<String> request = new HttpEntity<>(JO, headers);
         restTemplate.postForEntity(uri, request, String.class);
-
     }
 }
